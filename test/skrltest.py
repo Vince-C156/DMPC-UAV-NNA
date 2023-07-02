161dcb2b97c9f29bde8d471ab196b813cb0a6c02
Vince-C156 <vc168888@gmail.com>
2023-07-02 14:26:52 -0700

    testing folder

---
+++ test/skrltest.py
@@ -0,0 +1,18 @@
+# import the environment loader
+from skrl.envs.torch import load_omniverse_isaacgym_env
+from skrl.envs.torch import wrap_env
+
+print("INITALIZING SCRIPT")
+# load environment
+env = load_omniverse_isaacgym_env(task_name="Crazyflie")
+#env = wrap_env(env)
+obs, infos = env.reset()
+terminated, truncated = False, False
+print("================")
+print(f"DONE LOADING ENV x0 {obs}")
+print("================")
+while not (terminated or truncated):
+    env.render()
+    action = env.action_space.sample()
+    obs, reward, terminated, truncated, info = env.step(action)
+env.close()
